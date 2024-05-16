b674e160c90f96f43f6bfe182279611c3aa74e3e
dz1dzor <kbismarkkwaku@gmail.com>
2024-05-16 09:13:18 +0000

    Handled more than 2000 requests

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,17 @@
+# Increasing the number of traffics our nginx server can handle
+
+# Increase default file
+exec { 'fix--for-nginx':
+	# changing the ULIMIT value
+	command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+	# path specifier
+	path	=> '/usr/local/bin/:/bin/',
+}
+
+# restarting nginx
+exec { 'nginx-restart':
+	# restart nginx
+	command => '/etc/init.d/nginx restart',
+	# path specifier
+	path	=> '/etc/init.d/',
+}
