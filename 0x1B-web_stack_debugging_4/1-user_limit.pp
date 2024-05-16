a51ef65338645407e052a9d76d40b140f5b8dacf
dz1dzor <kbismarkkwaku@gmail.com>
2024-05-16 09:54:22 +0000

    Solution to task 1: handling user limits

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,15 @@
+# Allowing the user: holberton to log in and handle files without error
+
+# increasing limit for holberton
+exec { 'increase-hard-limit':
+  command => "sed -i '/^holberton hard/s/4/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin/:/bin/',
+}
+
+# increasing soft limit
+exec { 'increase-soft-limit':
+  command => "sed -i '/^holberton soft/s/5/50000/' /etc/security/limits.conf
+",
+  path    => '/usr/local/bin/:/bin/',
+}
+
