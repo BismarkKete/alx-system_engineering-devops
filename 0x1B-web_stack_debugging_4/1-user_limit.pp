7f60965c215743f26b8313626e13fa12480584d3
Bismark-K <kbismarkkwaku@gmail.com>
2024-05-16 10:30:06 +0000

    Debugged

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,13 @@
 # Handles user limit allowing holberton to log in and open files without error
 # Author: Bismark-K
 
-exec {'replace-1':
+exec {'change-soft-limit':
   provider => shell,
   command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
-  before   => Exec['replace-2'],
+  before   => Exec['change-hard-limit'],
 }
 
-exec {'replace-2':
+exec {'change-hard-limit':
   provider => shell,
   command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
 }
