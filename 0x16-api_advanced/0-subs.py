4d369a559c7e12f5d8b988d34615dc178206be4d
Bismark-K <kbismarkkwaku@gmail.com>
2024-06-05 10:31:34 +0000

    Debugged

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,12 +11,16 @@ import requests
 def number_of_subscribers(subreddit):
     "The function will return 0 when an invalid subreddit is given."
 
-    rqt = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Custom"}
 
-    if rqt.status_code == 200:
-        return rqt.json().get("data").get("subscribers")
+    rspns = requests.get(url, headers=headers, allow_redirects=False)
+
+    if rspns.status_code == 200:
+        try:
+            data = rspns.json()
+            return data.get("data").get("subscribers", 0)
+        except ValueError:
+            return 0
     else:
         return 0
